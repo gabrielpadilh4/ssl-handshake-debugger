eb17539d15cd8970fc520a0a3dc5fd443a15b063
Gabriel Padilha Santos <gabrielpadilhasantos@gmail.com>
2023-05-02 22:19:35 -0300

    feat: Added slf4j and code improved

--- src/main/java/io/github/gabrielpadilh4/services/SSLService.java
+++ src/main/java/io/github/gabrielpadilh4/services/SSLService.java
@@ -2,12 +2,18 @@ package io.github.gabrielpadilh4.services;
 
 import io.github.gabrielpadilh4.exceptions.InvalidUrlException;
 import io.github.gabrielpadilh4.models.CommandLineSSL;
+import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.File;
 import java.io.PrintStream;
+import java.net.Socket;
 import java.net.URL;
+import java.security.cert.X509Certificate;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 /**
@@ -16,8 +22,6 @@ import java.util.regex.Pattern;
 public class SSLService {
 
     private final static String URL_PATTERN = "^(https):\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$";
-    private final static int DEFAULT_TIMEOUT = 5000;
-    private final static String DEFAULT_HTTP_METHOD = "GET";
 
     public static void validateHttpsUrl(String url) throws InvalidUrlException {
         boolean isMatch = Pattern.compile(URL_PATTERN)
@@ -31,6 +35,7 @@ public class SSLService {
 
     public static void logSSLHandshake(CommandLineSSL commandLineSSL) {
         try {
+
             System.setProperty("javax.net.debug", "ssl:handshake");
 
             if (!commandLineSSL.getFileName().isBlank()) {
@@ -41,21 +46,38 @@ public class SSLService {
             }
 
             URL url = new URL(commandLineSSL.getUrl());
-            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
             SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
 
-            connection.setSSLSocketFactory(sslSocketFactory);
-            connection.setRequestMethod(DEFAULT_HTTP_METHOD);
-            connection.setReadTimeout(DEFAULT_TIMEOUT);
-            connection.setReadTimeout(DEFAULT_TIMEOUT);
-
-            int responseCode = connection.getResponseCode();
+            SSLSocket socket = (SSLSocket) sslSocketFactory.createSocket(url.getHost(), url.getDefaultPort());
+            SSLSession session = socket.getSession();
+            String cipherSuite = session.getCipherSuite();
+            X509Certificate[] peerCertificates = (X509Certificate[]) session.getPeerCertificates();
+            String protocolVersion = session.getProtocol();
 
             if (!commandLineSSL.getFileName().isBlank()) {
-                System.err.printf("\nResponse code %s for %s", responseCode, commandLineSSL.getUrl());
+                System.err.printf("Cipher suite: %s\n", cipherSuite);
+                System.err.printf("Protocol version: %s\n", protocolVersion);
+                System.err.print("Peer certificates:\n");
+                for (X509Certificate cert : peerCertificates) {
+                    System.err.printf("- Subject: %s\n", cert.getSubjectDN().getName());
+                    System.err.printf("  Issuer: %s\n", cert.getIssuerDN().getName());
+                    System.err.printf("  Valid from: %s\n", cert.getNotBefore().toString());
+                    System.err.printf("  Valid until: %s\n", cert.getNotAfter().toString());
+                }
             }
 
-            System.out.printf("\nResponse code %s for %s", responseCode, commandLineSSL.getUrl());
+            System.out.printf("Cipher suite: %s\n", cipherSuite);
+            System.out.printf("Protocol version: %s\n", protocolVersion);
+            System.out.print("Peer certificates:\n");
+            for (X509Certificate cert : peerCertificates) {
+                System.out.printf("- Subject: %s\n", cert.getSubjectDN().getName());
+                System.out.printf("  Issuer: %s\n", cert.getIssuerDN().getName());
+                System.out.printf("  Valid from: %s\n", cert.getNotBefore().toString());
+                System.out.printf("  Valid until: %s\n", cert.getNotAfter().toString());
+            }
+
+            socket.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
